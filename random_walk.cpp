e00ef5c6f9d0583bf834f9fff736347ed11d615a
vimalpmenon7 <vimalparakkal7@gmail.com>
2025-08-06 13:49:54 +0530

    New modified random_walk.cpp file

--- random_walk.cpp
+++ random_walk.cpp
@@ -49,68 +49,17 @@ int main(int argc, char **argv)
     return 0;
 }
 
-#include <iostream>
-#include <cstdlib> // For atoi, rand, srand
-#include <ctime>   // For time
-#include <mpi.h>
-
-void walker_process();
-void controller_process();
-
-int domain_size;
-int max_steps;
-int world_rank;
-int world_size;
-
-int main(int argc, char **argv)
-{
-    // Initialize the MPI environment
-    MPI_Init(&argc, &argv);
-
-    // Get the number of processes and the rank of this process
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-    if (argc != 3)
-    {
-        if (world_rank == 0)
-        {
-            std::cerr << "Usage: mpirun -np <p> " << argv[0] << " <domain_size> <max_steps>" << std::endl;
-        }
-        MPI_Finalize();
-        return 1;
-    }
-
-    domain_size = atoi(argv[1]);
-    max_steps = atoi(argv[2]);
-
-    if (world_rank == 0)
-    {
-        // Rank 0 is the controller
-        controller_process();
-    }
-    else
-    {
-        // All other ranks are walkers
-        walker_process();
-    }
-
-    // Finalize the MPI environment
-    MPI_Finalize();
-    return 0;
-}
-
 void walker_process()
 {
     srand(time(NULL) + world_rank);
-    int posn = 0;
+    int position = 0;
 
     for (int step = 1; step <= max_steps; ++step)
     {
         int move = (rand() % 2 == 0) ? -1 : 1;
-        posn += move;
+        position += move;
 
-        if (posn < -domain_size || posn > domain_size)
+        if (position < -domain_size || position > domain_size)
         {
             std::cout << "Rank " << world_rank << ": Walker finished in " << step << ".\n";
             int done_signal = step;
@@ -127,15 +76,14 @@ void walker_process()
 
 void controller_process()
 {
-    int no_walkers = world_size - 1;
+    int num_walkers = world_size - 1;
 
     for (int i = 0; i < num_walkers; ++i)
     {
-        int rsteps;
+        int received_steps;
         MPI_Status status;
-        MPI_Recv(&rsteps, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(&received_steps, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
     }
 
-    std::cout << "Controller: All " << no_walkers << " walkers have finished.\n";
+    std::cout << "Controller: All " << num_walkers << " walkers have finished.\n";
 }
-
